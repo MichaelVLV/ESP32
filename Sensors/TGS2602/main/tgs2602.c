01c9418fb2e461efcadcde048807a91b526f716c
Michael <michaellbnv@gmail.com>
2018-01-21 18:30:10 +0200

    added Rs/R0 output

--- Sensors/TGS2602/main/tgs2602.c
+++ Sensors/TGS2602/main/tgs2602.c
@@ -4,53 +4,59 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 #include "esp_adc_cal.h"
 #include "driver/adc.h"
 
 #define BLINK_GPIO 2
-#define V_REF   1116  					   // DOIT board
-#define ADC1_ADV_CHANNEL (ADC1_CHANNEL_6)  //GPIO 34
+#define V_REF   1116                         // reference voltage on esp-DOIT board
+#define ADC1_ADV_CHANNEL (ADC1_CHANNEL_6)  // GPIO 34
 
-// testing task
-void blink_task(void *pvParameter)
+// read data from tgs
+void TGS2602_getData(uint32_t voltage_out)
 {
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
-    gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
-        /* Blink off (output low) */
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-    }
+    const uint16_t Rload = 1000; // Rload = 1kOhm (value of load resistor on schematic, min. value 450 Ohm)
+    uint16_t Rzero = 33800;      // experimental value of R0 (at clean air)
+                                 // this value may be in range 10k to 100k, adjust to your sensor
+
+    float Vcc = 4.7;    // Vcc (actual supply voltage @ 5V pins)
+
+    float v_Rload = voltage_out / 1000.0; // voltage on Rload (in V)
+    printf("v_Rload:%.2f (V)\n", v_Rload);
+
+    float Rsensor = ((Vcc * Rload) / v_Rload ) - Rload; // resistance on Rs, formula as noted in datasheet:
+                                                        // Rs = (Vc * Rl) / Vout - Rl
+    printf("Rsensor:%.1f (Ohm)\n", Rsensor);
+
+    float VOC = Rsensor / Rzero;    // Rs/R0 coefficient, may vary a lot due Rzero value
+    printf("VOC (Rs / R0) coef:%.3f\n", VOC);
+
+    // ToDo:approximated formula from Gas Concentration graph in datasheet
+    // float VOC_ppm = (7.3 - 9 * VOC) / 0.55;
+    // printf("VOC:%.2f (ppm)\n", VOC_ppm);
 }
 
-void adc_task (void *pvParamter)
+// rtos task
+void TGS2602_task (void *pvParamter)
 {
     esp_adc_cal_characteristics_t characteristics;
     adc1_config_width(ADC_WIDTH_12Bit);
-    adc1_config_channel_atten(ADC1_ADV_CHANNEL, ADC_ATTEN_11db);
-    esp_adc_cal_get_characteristics(V_REF, ADC_ATTEN_11db, ADC_WIDTH_12Bit, &characteristics);
+    adc1_config_channel_atten(ADC1_ADV_CHANNEL, ADC_ATTEN_DB_6);
+    esp_adc_cal_get_characteristics(V_REF, ADC_ATTEN_DB_6, ADC_WIDTH_12Bit, &characteristics);
     uint32_t voltage;
     while(1)
     {
         voltage = adc1_to_voltage(ADC1_ADV_CHANNEL, &characteristics);
-        printf("ADC(GPIO34):Vin:%d (mV)\n",voltage);
+        uint32_t adc_raw = adc1_get_raw (ADC1_ADV_CHANNEL);
+        printf("ADC:%d (mV) , raw: %d\n",voltage, adc_raw);
+
+        TGS2602_getData(voltage);
+
+        printf("***************\n");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
 void app_main()
 {
-    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE,   NULL, 5, NULL);
-    xTaskCreate(&adc_task,   "adc_task",   configMINIMAL_STACK_SIZE*2, NULL, 5, NULL);
+    xTaskCreate(&TGS2602_task,   "TGS2602_task",   configMINIMAL_STACK_SIZE*2, NULL, 5, NULL);
 }
